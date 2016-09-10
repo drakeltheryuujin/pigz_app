d6af7d72874a98ee84738230969e647d8916f5b5
Gust <github email address>
2016-09-10 17:14:37 -0400

    adds validations

--- app/controllers/api/v1/cops_controller.rb
+++ app/controllers/api/v1/cops_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class CopsController < BaseController
+      skip_before_action :verify_content_type_header
+
     end
   end
 end
--- app/models/cop.rb
+++ app/models/cop.rb
@@ -1,4 +1,9 @@
 class Cop < ActiveRecord::Base
   validates :latitude, presence: true
   validates :longitude, presence: true
+  validates :racism_index, presence: true
+  validates :gender, presence: true
+  validates :skin_color, presence: true
+  validates :body_type, presence: true
+
 end
