5b91225c3c1d478d00eb1b9890f9d97bc80289c4
Adam Goldstein <abgoldstein@gmail.com>
2016-09-10 20:24:57 -0400

    Fix Cop create action

--- app/controllers/api/v1/cops_controller.rb
+++ app/controllers/api/v1/cops_controller.rb
@@ -2,10 +2,6 @@ module Api
   module V1
     class CopsController < BaseController
       skip_before_action :verify_content_type_header
-
-      def create
-        #when new cop is created, show push notification
-      end
     end
   end
 end
