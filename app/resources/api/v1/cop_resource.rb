ee4bbed74c050dee430685859da0c16706fc2e37
Adam Goldstein <abgoldstein@gmail.com>
2016-09-10 20:18:11 -0400

    Drop deprecated columns from Cop resource

--- app/resources/api/v1/cop_resource.rb
+++ app/resources/api/v1/cop_resource.rb
@@ -1,9 +1,5 @@
 class Api::V1::CopResource < JSONAPI::Resource
   attribute :latitude
   attribute :longitude
-  attribute :racism_index
-  attribute :gender
-  attribute :skin_color
-  attribute :body_type
-
+  attribute :demeanor
 end
