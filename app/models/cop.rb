9f31334fb3716a02d8b4ec35ce4c7efdd7332e8b
Adam Goldstein <abgoldstein@gmail.com>
2016-09-12 16:01:51 -0400

    Remove geokit from Cop

--- app/models/cop.rb
+++ app/models/cop.rb
@@ -2,10 +2,4 @@ class Cop < ActiveRecord::Base
   validates :latitude, presence: true
   validates :longitude, presence: true
   validates :demeanor, presence: true
-
-  acts_as_mappable default_units: :kms,
-                   default_formula: :sphere,
-                   distance_field_name: :distance,
-                   lat_column_name: :latitude,
-                   lng_column_name: :longitude
 end
